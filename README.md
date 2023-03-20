fcf0797972a67dca75ccd6561b666e5280a0e821
SharpIce <VupRbl@163.com>
2023-03-21 01:56:59 +0800

    初始提交
    签字人：SharpIce <VupRbl@163.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# 锐冰 URL 中转站
+
+## 介绍
+
+这是一个简单的 URL 中转站项目
+
+## 鸣谢
+
+[Gradient Colors Collection Palette - CoolHue 2.0](https://webkul.github.io/coolhue/) **提供配色支持**
---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("欢迎使用SharpIce URL Transit！");
+
+var Index = {
+    IDJump: function (DivID) {
+        window.location.assign(window.location.origin + "/index.html#" + DivID)
+    }
+}
